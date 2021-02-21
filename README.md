2d274ab297b8bd29d714c8c9cbabc239a36b1416
SagaLinZP <sagalinzp@gmail.com>
2021-02-21 22:05:01 +0800

    main

--- README.md
+++ README.md
@@ -1,6 +1,41 @@
 # HNU-Auto-Clockin
-HNU每日自动打卡
 
-参考开源仓库：
+HNU疫情防控和健康监测系统每日自动打卡
 
-[中南大学nCov健康打卡定时自动脚本](https://github.com/lxy764139720/Auto_Attendance)
+**使用前须知：本项目仅用于学习交流Python语言的学习与相关库的使用，如有体温异常等请如实上报！**
+
+参考开源仓库：[中南大学nCov健康打卡定时自动脚本](https://github.com/lxy764139720/Auto_Attendance)
+
+Duang~~~在GayHub里面找了一圈才看到一个湖大的打卡脚本，作者还不更新了，可能是信息院的同学都不想开源吧😅😅😅。于是在极度愤怒的情况下自己生了个玩意。
+
+## 食用方法
+
+本品通过GitHub Actions实现自动化，~~具有美容养颜、改善睡眠、舒肝理气、应该不会再被辅导员电话轰炸或被喝茶等功效。~~ 需要你做的前期工作有：
+
+1. **将本项目fork到你自己的GitHub账号**
+
+2. **配置账号密码**
+
+    进入刚刚你fork过去到自己名下的项目，再进入Settings -> Secrets页面，点击New repository Secret，在Name栏输入**USERNAME**，Value栏输入你的学号。然后再添加一个Secret，Name栏为**PASSWORD**，Value栏填写你登录个人中心的密码。
+
+3. **填写打卡地址**
+
+    你还需要分别以**PROVINCE**, **CITY**, **COUNTY**为NAME添加相应的Secret，Value中请注意须在地名后面添加“省“、”市“、”县/区“，如”湖南省“、”长沙市“、”岳麓区“。另外，详细地址默认为一个句号，你可以在源代码中修改。
+
+4. **配置验证码识别API**
+
+    由于不在微信环境下登录打卡系统需要验证码，所以需要使用网上的API来自动识别，请参考如下文章：
+
+    [验证码识别的免费 OCR](https://www.cnblogs.com/xiaowenshu/p/11792012.html)
+
+    按照文章操作注册百度智能云账号，拿到**AppID**, **API Key**, **Secret Key**三个参数，再回到GitHub的Secrets界面，分别以**APP_ID**, **API_KEY**, **SECRET_KEY**为NAME创建三个SECRETS。
+
+5. **开始自动化运行**
+
+    进入到**Actions**界面，点击该工作流，然后Run workflow，即可开启自动化运行，你可以在设置里绑定邮箱以接收运行失败的通知，防止未来哪天打卡系统升级了你还蒙在鼓里。
+
+    我设定为每天早晨6：10自动运行，你可以在/.github/workflows/python-app.yml文件里修改，请注意cron语法下的时间为零时区时间，需要将北京时间减8个小时，且分钟在前小时在后。
+
+6. **如果一切顺利的话，应该没有第六步。若不是那样，可以在Discussion里面说。**
+
+## 觉得有用的话给👴点个Star呗~
--- clock_in.py
+++ clock_in.py
@@ -3,9 +3,12 @@ import requests
 import json
 import argparse
 
-parser = argparse.ArgumentParser(description='manual to this script')
+parser = argparse.ArgumentParser()
 parser.add_argument('--username', type=str, default=None)
 parser.add_argument('--password', type=str, default=None)
+parser.add_argument('--province', type=str, default=None)
+parser.add_argument('--city', type=str, default=None)
+parser.add_argument('--county', type=str, default=None)
 parser.add_argument('--app_id', type=str, default=None)
 parser.add_argument('--api_key', type=str, default=None)
 parser.add_argument('--secret_key', type=str, default=None)
@@ -13,39 +16,47 @@ args = parser.parse_args()
 
 OCRClient = AipOcr(args.app_id, args.api_key, args.secret_key)
 
-getimgvcode = json.loads(requests.get('https://fangkong.hnu.edu.cn/api/v1/account/getimgvcode').text)['data']['Token']
-captcha = OCRClient.basicGeneralUrl(f'https://fangkong.hnu.edu.cn/imagevcode?token={getimgvcode}')['words_result'][0]['words']
-login_info = {"Code":args.username,"Password":args.password,"VerCode":captcha,"Token":getimgvcode}
+def main():
+    getimgvcode = json.loads(requests.get('https://fangkong.hnu.edu.cn/api/v1/account/getimgvcode').text)['data']['Token']
+    captcha = OCRClient.basicGeneralUrl(f'https://fangkong.hnu.edu.cn/imagevcode?token={getimgvcode}')['words_result'][0]['words']
+    if len(captcha) != 4:
 
-login_url = 'https://fangkong.hnu.edu.cn/api/v1/account/login'
-set_cookie = requests.post(login_url, json=login_info)
-ASPXAUTH = set_cookie.headers['Set-Cookie'][702:-8]
-access_token = json.loads(set_cookie.text)['data']['AccessToken']
+    login_info = {"Code":args.username,"Password":args.password,"VerCode":captcha,"Token":getimgvcode}
 
-clockin_url = 'https://fangkong.hnu.edu.cn/api/v1/clockinlog/add'
-headers = {'Cookie': f'{ASPXAUTH}; TOKEN={getimgvcode}; Hm_lvt_d7e34467518a35dd690511f2596a570e=1612281837,1613093402,1613146382; pgv_pvi=4032871424'}
-clockin_data = {"Temperature":"null",
-                "RealProvince":"福建省",
-                "RealCity":"泉州市",
-                "RealCounty":"泉港区",
-                "RealAddress":"。",
-                "IsUnusual":"0",
-                "UnusualInfo":"",
-                "IsTouch":"0",
-                "IsInsulated":"0",
-                "IsSuspected":"0",
-                "IsDiagnosis":"0",
-                "tripinfolist":[{"aTripDate":"","FromAdr":"","ToAdr":"","Number":"","trippersoninfolist":[]}],
-                "toucherinfolist":[],
-                "dailyinfo":{"IsVia":"0","DateTrip":""},
-                "IsInCampus":"0",
-                "IsViaHuBei":"0",
-                "IsViaWuHan":"0",
-                "InsulatedAddress":"",
-                "TouchInfo":"",
-                "IsNormalTemperature":"1",
-                "Longitude":118.95317077636719,
-                "Latitude":25.116409301757812}
+    login_url = 'https://fangkong.hnu.edu.cn/api/v1/account/login'
+    set_cookie = requests.post(login_url, json=login_info)
+    ASPXAUTH = set_cookie.headers['Set-Cookie'][702:-8]
+    access_token = json.loads(set_cookie.text)['data']['AccessToken']
 
-clockin = requests.post(clockin_url, headers=headers, json=clockin_data)
-print(clockin.text)
+    clockin_url = 'https://fangkong.hnu.edu.cn/api/v1/clockinlog/add'
+    headers = {'Cookie': f'{ASPXAUTH}; TOKEN={getimgvcode}; Hm_lvt_d7e34467518a35dd690511f2596a570e=1612281837,1613093402,1613146382; pgv_pvi=4032871424'}
+    clockin_data = {"Temperature":"null",
+                    "RealProvince":province,
+                    "RealCity":city,
+                    "RealCounty":county,
+                    "RealAddress":"。",
+                    "IsUnusual":"0",
+                    "UnusualInfo":"",
+                    "IsTouch":"0",
+                    "IsInsulated":"0",
+                    "IsSuspected":"0",
+                    "IsDiagnosis":"0",
+                    "tripinfolist":[{"aTripDate":"","FromAdr":"","ToAdr":"","Number":"","trippersoninfolist":[]}],
+                    "toucherinfolist":[],
+                    "dailyinfo":{"IsVia":"0","DateTrip":""},
+                    "IsInCampus":"0",
+                    "IsViaHuBei":"0",
+                    "IsViaWuHan":"0",
+                    "InsulatedAddress":"",
+                    "TouchInfo":"",
+                    "IsNormalTemperature":"1",
+                    "Longitude":118.95317077636719,
+                    "Latitude":25.116409301757812}
+
+    clockin = requests.post(clockin_url, headers=headers, json=clockin_data)
+    if '成功' in clockin.text or '已提交' in clockin.text:
+        isSucccess = 0
+    else:
+        isSucccess = 1
+
+main()
