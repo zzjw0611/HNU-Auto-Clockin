7b6869126235a1970292afa844e5873d6a0d13b0
SagaLinZP <75353202+SagaLinZP@users.noreply.github.com>
2022-02-20 19:08:23 +0800

    Update clock_in.py

--- clock_in.py
+++ clock_in.py
@@ -77,7 +77,7 @@ def main():
                         "IsVia": "0",
                         "DateTrip": ""
                     },
-                    "IsInCampus": "0",
+                    "IsInCampus": "1",
                     "IsViaHuBei": "0",
                     "IsViaWuHan": "0",
                     "InsulatedAddress": "",
