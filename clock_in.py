8b27e852722927aa4f9e4d66de0d0ab9549e3b31
SagaLinZP <75353202+SagaLinZP@users.noreply.github.com>
2022-02-24 09:23:29 +0800

    Update clock_in.py

--- clock_in.py
+++ clock_in.py
@@ -53,39 +53,18 @@ def main():
     headers = login()
     lon, lat, real_address = setLocation()
     clockin_data = {
-                    "Temperature": "null",
-                    "RealProvince": args.province,
-                    "RealCity": args.city,
-                    "RealCounty": args.county,
-                    "RealAddress": real_address,
-                    "IsUnusual": "0",
-                    "UnusualInfo": "",
-                    "IsTouch": "0",
-                    "QRCodeColor": "绿色",
-                    "IsInsulated": "0",
-                    "IsSuspected": "0",
-                    "IsDiagnosis": "0",
-                    "tripinfolist": [{
-                        "aTripDate": "",
-                        "FromAdr": "",
-                        "ToAdr": "",
-                        "Number": "",
-                        "trippersoninfolist": []
-                    }],
-                    "toucherinfolist": [],
-                    "dailyinfo": {
-                        "IsVia": "0",
-                        "DateTrip": ""
-                    },
-                    "IsInCampus": "1",
-                    "IsViaHuBei": "0",
-                    "IsViaWuHan": "0",
-                    "InsulatedAddress": "",
-                    "TouchInfo": "",
-                    "IsNormalTemperature": "1",
-                    "Longitude": lon,
-                    "Latitude": lat
-                }
+                    "Longitude":lon,
+                    "Latitude":lat,
+                    "RealProvince":args.province,
+                    "RealCity":args.city,
+                    "RealCounty":args.county,
+                    "RealAddress":real_address,
+                    "BackState":1,
+                    "MorningTemp":"36.5",
+                    "NightTemp":"36.5",
+                    "tripinfolist":[],
+                    "QRCodeColor":"绿色"
+                    }
 
     clockin = requests.post(clockin_url, headers=headers, json=clockin_data)
 
